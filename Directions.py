88c034e3d999db14a60f8d099aa2b940df15cbec
Thomas Cooper <tom.n.cooper@gmail.com>
2015-04-14 22:58:00 +0100

    Added postcode regex pattern checking for input

--- Directions.py
+++ Directions.py
@@ -2,17 +2,17 @@ import requests
 import csv
 import time
 import datetime
-import os.path
+import re
 
 def get_dist_duration(UniqueID, start, end, api_key):
     """ Gets distance and duration of a journey between start and end postcodes for driving, cycling and walking.
-    
+
     Arguements:
         UniqueID - String - The unique ID number for this postcode start-end pair
         start - String - Origin postcode
         end - String - Destination postcode
         api_key - String - The google_api key to be used for the request
-        
+
     Returns:
         A dictionary containing the origin and destination postcodes and the distance and duration for each mode of transport
         Dictionary keys:
@@ -22,15 +22,15 @@ def get_dist_duration(UniqueID, start, end, api_key):
             "Driving Duration (sec)",
             "Bicycling Distance (m)",
             "Bicycling Duration (sec)",
-            "Walking Distance (m)", 
+            "Walking Distance (m)",
             "Walking Duration (sec)"
-    """    
+    """
     #Array containing modes of transport for which information is requested - transit handled separately below as output is more complex
     modes = ('driving', 'bicycling', 'walking')
-    
+
     #Set up a list containing the column header names for each of the distance and duration values to be added as keys to the values dictionary
     colnames = (("Driving Distance (m)","Driving Duration (sec)"),("Bicycling Distance (m)", "Bicycling Duration (sec)"),("Walking Distance (m)", "Walking Duration (sec)"))
-    
+
     #The dictionary that will store distance and duration for the start and end postcodes supplied for each travel mode
     values = dict()
 
@@ -41,10 +41,10 @@ def get_dist_duration(UniqueID, start, end, api_key):
 
     #Cycle through the transport modes and get the directions for each
     for i, mode in enumerate(modes):
-        
+
         #Make the request to the Google Directions API
         r = requests.get('https://maps.googleapis.com/maps/api/directions/json?origin={}&destination={}&mode={}&key={}'.format(start, end, mode, api_key))
-        
+
         #If the request is not successful print an error
         if r.status_code != 200:
             print "Request error"
@@ -60,59 +60,59 @@ def get_dist_duration(UniqueID, start, end, api_key):
         time.sleep(0.5)
 
     return values
-    
+
 def get_departure_time(departure_time, weekday = 2):
-    """ Gets the number of seconds since the epoch (midnight 01/01/1970) to the next weekday (wednesday as default as this is unlikly to be a public holiday) at the provided time. 
-    
+    """ Gets the number of seconds since the epoch (midnight 01/01/1970) to the next weekday (wednesday as default as this is unlikly to be a public holiday) at the provided time.
+
     Arguments:
         departure_time - String - Time of day in the format HH:mm:ss
-        weekday - Integer - The day of the week to set the datetime object to. Monday = 0, Sunday = 6 
-        
+        weekday - Integer - The day of the week to set the datetime object to. Monday = 0, Sunday = 6
+
     Returns:
         Integer - The number of seconds since the epoch (midnight 01/01/1970) to the next monday at the provided time
-    """    
+    """
     #Get a datetime object for now
     now = datetime.datetime.now()
-    
+
     #Calculate the number of days ahead of the required day of the week we are
     days_ahead = weekday - now.weekday()
-    
+
     #If we have passed the required day of the week increase the days ahead to account for this
     if days_ahead <= 0 :
         days_ahead = days_ahead + 7
-    
+
     #Get a datetime object for the next required weekday
     next_weekday = now + datetime.timedelta(days=days_ahead)
-    
+
     #Get the hours minutes and seconds from the depature time string
     departure_time = departure_time.split(":")
     hour = int(departure_time[0])
     minutes = int(departure_time[1])
     seconds = int(departure_time[2])
-    
+
     #Get a datetime object for the next required weekday at the required time
     new_date = next_weekday.replace(hour=hour, minute=minutes, second=seconds)
-    
+
     #Get a datetime object for the epoch
     epoch = datetime.datetime(1970,1,1)
-    
+
     #Get the difference between the new date and the epoch in seconds
     dt_secs = int((new_date - epoch).total_seconds())
 
     print "Depature Time:"
     print datetime.datetime.fromtimestamp(dt_secs)
-    
+
     return dt_secs
-    
+
 def get_transit_details(start, end, api_key, departure_time = None):
-    """ Gets direction information (see dictionary keys) for public transport between the supplied start and end postcodes. This method assumes no intermediat waypoints and uses the 1st returned route. 
-    
+    """ Gets direction information (see dictionary keys) for public transport between the supplied start and end postcodes. This method assumes no intermediat waypoints and uses the 1st returned route.
+
     Arguements:
         start - String - Origin postcode
         end - String - Destination postcode
         api_key - String - The google_api key to be used for the request
-        departure_time - Integer - The number of seconds since the epoch (midnight 01/01/1970) the default is the current time 
-        
+        departure_time - Integer - The number of seconds since the epoch (midnight 01/01/1970) the default is the current time
+
     Returns:
         A dictionary containing the origin and destination postcodes and the distance and duration for each mode of transport
         Dictionary keys:
@@ -123,10 +123,10 @@ def get_transit_details(start, end, api_key, departure_time = None):
             "Walking Distance from last stop (m)",
             "Total Walking Distance (m)"
     """
-       
+
     #The dictionary that will store the transit information for the supplied origin and destination
     values = dict()
-    
+
     #Make the request to the Google Directions API
     if departure_time:
         #If a depature time is provided use it
@@ -134,7 +134,7 @@ def get_transit_details(start, end, api_key, departure_time = None):
     else:
         #If no depature time is provided use the current time (this is the default api behaviour if not time is provided)
         r = requests.get('https://maps.googleapis.com/maps/api/directions/json?origin={}&destination={}&mode=transit&key={}'.format(start, end, api_key))
-    
+
     if r.status_code != 200:
         print "Request Error"
     else:
@@ -155,7 +155,7 @@ def get_transit_details(start, end, api_key, departure_time = None):
             values["Walking Distance to 1st stop (m)"] = steps[0].get('distance').get('value')
         else:
             values["Walking Distance to 1st stop (m)"] = 0.0
-        
+
         if steps[len(steps)-1].get('travel_mode') == 'WALKING':
             values["Walking Distance from last stop (m)"] = steps[len(steps)-1].get('distance').get('value')
         else:
@@ -163,7 +163,7 @@ def get_transit_details(start, end, api_key, departure_time = None):
 
         #Calculate TOTAL walking distance involved in transit route. Start by setting the total walking distance to zero.
         walking_dist = 0.0
-        
+
         #For each step that Google labels as "WALKING" add the distance to the prior total walking from above.
         for step in steps:
             if step.get('travel_mode') == 'WALKING':
@@ -178,15 +178,15 @@ def get_transit_details(start, end, api_key, departure_time = None):
     return values
 
 def get_direction_data(UniqueID, start, end, api_key, depature_time = None):
-    """ Gets direction information (see dictionary keys) for various transport methods between the supplied start and end postcodes. This method assumes no intermediat waypoints and uses the 1st returned route. 
-    
+    """ Gets direction information (see dictionary keys) for various transport methods between the supplied start and end postcodes. This method assumes no intermediat waypoints and uses the 1st returned route.
+
     Arguements:
         UniqueID - String - The unique ID number for this postcode start-end pair
         start - String - Origin postcode
         end - String - Destination postcode
         api_key - String - The google_api key to be used for the request
-        departure_time - Integer - The number of seconds since the epoch (midnight 01/01/1970) the default is the current time 
-        
+        departure_time - Integer - The number of seconds since the epoch (midnight 01/01/1970) the default is the current time
+
     Returns:
         A dictionary containing the origin and destination postcodes and the distance and duration for each mode of transport
         Dictionary keys:
@@ -196,7 +196,7 @@ def get_direction_data(UniqueID, start, end, api_key, depature_time = None):
             "Driving Duration (sec)",
             "Bicycling Distance (m)",
             "Bicycling Duration (sec)",
-            "Walking Distance (m)", 
+            "Walking Distance (m)",
             "Walking Duration (sec)"
             "Transit Distance (m)",
             "Transit Duration (sec)",
@@ -205,20 +205,20 @@ def get_direction_data(UniqueID, start, end, api_key, depature_time = None):
             "Walking Distance from last stop (m)",
             "Total Walking Distance (m)"
     """
-    
+
     #Get the indervidual dictionaries for the different transport modes
     vals = get_dist_duration(UniqueID, start, end, api_key)
     trans = get_transit_details(start, end, api_key, depature_time)
-    
+
     #Combine them into one dictionary
     data = vals.copy()
     data.update(trans)
-    
+
     return data
 
 def write_to_csv(filename, row):
     """ Writes a list of direction results to a csv file.
-    
+
     Arguements:
         filename - String - name of the file to write the results too.
         row - dictionary - dictionary containing the direction results for a post code pair
@@ -228,24 +228,39 @@ def write_to_csv(filename, row):
         exists = True
     else:
         exists = False
-        
+
 def read_postcode_csv(filename):
     """ Reads a csv file and returns a list of dictionary objects with the column headers as keys.
-    
+
     Arguments:
         filename - String - The filename of the csv file that is to be opened
-        
+
     Returns:
         A list of dictionary objects, eack of which represents a row of the csv with the keys as the column names.
     """
     #Create the data list
     data = list()
-    
+
     with open(filename) as csvfile:
         reader = csv.DictReader(csvfile)
-        
+
         for row in reader:
             data.append(row)
-    
+
     return data
-    
+
+def check_postcode(postcode):
+    """ Checks if the supplied string matches the pattern for a UK post code. Note that this method does not actually check if a postcode actually exists. This method also expects a space in the middle of the postcode.
+
+    Arguments:
+        postcode - String - A string to be checked against the postcode pattern
+
+    Returns:
+        Boolean indicating if the supplied string contains a valid postcode pattern.
+    """
+    pattern = "^(?:(?:[A-PR-UWYZ][0-9]{1,2}|[A-PR-UWYZ][A-HK-Y][0-9]{1,2}|[A-PR-UWYZ][0-9][A-HJKSTUW]|[A-PR-UWYZ][A-HK-Y][0-9][ABEHMNPRV-Y])? [0-9][ABD-HJLNP-UW-Z]{2}|GIR 0AA)$"
+
+    if re.match(pattern, postcode.upper()):
+        return True
+    else:
+        return False
--- GetData.py
+++ GetData.py
@@ -7,11 +7,11 @@ config = ConfigParser.SafeConfigParser()
 config.read("Settings.cfg")
 
 #Get the API key from the settings file
-api_key = config.get('API', 'key') 
+api_key = config.get('API', 'key')
 
 #Get the input and output files from the settings file
-input_filename = config.get('Files', 'input') 
-output_filename = config.get('Files', 'output') 
+input_filename = config.get('Files', 'input')
+output_filename = config.get('Files', 'output')
 
 #Set the depature time for transit directions
 departure_time = get_departure_time("08:00:00")
@@ -24,25 +24,33 @@ total = len(inputs)
 
 #Open the output csv file for as long as it is needed
 with open(output_filename, 'wb') as csvfile:
-    
+
     #Set the header list
     fieldnames = ["UniqueID", "Origin Postcode", "Destination Postcode", "Driving Distance (m)", "Driving Duration (sec)","Bicycling Distance (m)", "Bicycling Duration (sec)", "Walking Distance (m)", "Walking Duration (sec)", "Transit Distance (m)", "Transit Duration (sec)", "Number of Transit Nodes", "Walking Distance to 1st stop (m)", "Walking Distance from last stop (m)", "Total Walking Distance (m)"]
-    
+
     #Create the writer object
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames, restval='NA')
+
     #Write the header containing the column names to the csv file
     writer.writeheader()
 
-    #Get a directions dictionary for each set of postcodes and save them to the output csv 
+    #Get a directions dictionary for each set of postcodes and save them to the output csv
     for i, item in enumerate(inputs):
-        print "Processing id: {} ({} of {})".format(item["UniqueID"], i+1, total) 
-        
+        print "Processing id: {} ({} of {})".format(item["UniqueID"], i+1, total)
+
         #Get the directions results from the api
-        result = get_direction_data(item["UniqueID"], item["OriginPostcode"], item["DestinationPostcode"], api_key, departure_time)
-        
+        origin = item["OriginPostcode"]
+        destination = item["DestinationPostcode"]
+
+        #Check the post codes and if both are fine submit to the api if not insert "Not Valid"
+        if check_postcode(origin) and check_postcode(destination):
+            result = get_direction_data(item["UniqueID"], origin, destination, api_key, departure_time)
+        elif check_postcode(origin) and not check_postcode(destination):
+            result = {"UniqueID":item["UniqueID"], "Origin Postcode":item["Origin Postcode"], "Destination Postcode":"Not Valid"}
+        elif not check_postcode(origin) and check_postcode(destination):
+            result = {"UniqueID":item["UniqueID"], "Origin Postcode":"Not Valid", "Destination Postcode":item["Destination Postcode"]}
+        else:
+            result = {"UniqueID":item["UniqueID"], "Origin Postcode":"Not Valid", "Destination Postcode":"Not Valid"}
+
         #Write the dictionary to the csv file
         writer.writerow(result)
-    
-
-
